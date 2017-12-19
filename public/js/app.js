dd744023896ccaba53a0ff172d1c1c0512574a59
Frank Köhn <frankkohn@Franks-MBP.SpeedportEntry2090126250021>
2017-12-19 14:31:38 +0100

    Fix

--- public/js/app.js
+++ public/js/app.js
@@ -47927,15 +47927,7 @@ var render = function() {
     _vm._l(_vm.messages, function(message) {
       return _c("li", { staticClass: "left clearfix" }, [
         _c("div", { staticClass: "chat-body clearfix" }, [
-          _c("div", { staticClass: "header" }, [
-            _c("strong", { staticClass: "primary-font" }, [
-              _vm._v(
-                "\n                    " +
-                  _vm._s(message.user.name) +
-                  "\n                "
-              )
-            ])
-          ]),
+          _vm._m(0, true),
           _vm._v(" "),
           _c("p", [
             _vm._v(
@@ -47947,7 +47939,16 @@ var render = function() {
     })
   )
 }
-var staticRenderFns = []
+var staticRenderFns = [
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", { staticClass: "header" }, [
+      _c("strong", { staticClass: "primary-font" })
+    ])
+  }
+]
 render._withStripped = true
 module.exports = { render: render, staticRenderFns: staticRenderFns }
 if (false) {
