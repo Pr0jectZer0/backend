79fbd858d7c6f25250b7024eee509b8cb25a8c75
Frank Köhn <frankkohn@Franks-MBP.SpeedportEntry2090126250021>
2017-12-19 15:08:55 +0100

    fix

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -19,6 +19,7 @@
     var pusher = new Pusher('60b89a1e182fd4635842', {
         cluster: 'eu',
         encrypted: true,
+        authEndpoint: 'https://pr0jectzer0.ml/broadcasting/auth',
         auth: {
             headers: {
                 'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEsImlzcyI6Imh0dHBzOi8vcHIwamVjdHplcjAubWwvYXBpL3VzZXIvbG9naW4iLCJpYXQiOjE1MTM2ODgwNzEsImV4cCI6MTUxMzY5MTY3MSwibmJmIjoxNTEzNjg4MDcxLCJqdGkiOiJXQlJjZkdWcmVsZHZjMXE1In0.jiRlAAVAGs2OiXCXc0MStUoAywUYffbpHWlxgXHZ6qc'
