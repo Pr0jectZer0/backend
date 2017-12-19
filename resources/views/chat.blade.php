c7300f4702d3d74bd0fdd0617fb6b420d5223c7f
Frank Köhn <frankkohn@Franks-MBP.SpeedportEntry2090126250021>
2017-12-19 14:55:26 +0100

    fix

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -1,21 +1,37 @@
-@extends('layouts.app')
-
-@section('content')
-
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Chats</div>
-
-                    <div class="panel-body">
-                        <chat-messages :messages="messages"></chat-messages>
-                    </div>
-                    <div class="panel-footer">
-
-                    </div>
-                </div>
-            </div>
-        </div>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Talking with Pusher</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        <h1>Laravel 5 and Pusher is fun!</h1>
+        <ul id="messages" class="list-group">
+        </ul>
     </div>
-@endsection
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="https://js.pusher.com/3.1/pusher.min.js"></script>
+<script>
+    //instantiate a Pusher object with our Credential's key
+    var pusher = new Pusher('60b89a1e182fd4635842', {
+        cluster: 'eu',
+        encrypted: true
+    });
+
+    //Subscribe to the channel we specified in our Laravel Event
+    var channel = pusher.subscribe('chat.2');
+
+    //Bind a function to a Event (the full Laravel class)
+    channel.bind('App\\Events\\MessageSent', addMessage);
+
+    function addMessage(data) {
+        var listItem = $("<li class='list-group-item'></li>");
+        listItem.html(data.message);
+        $('#messages').prepend(listItem);
+    }
+</script>
+</body>
+</html>
