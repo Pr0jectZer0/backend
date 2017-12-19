953a6eadf31fa1376cc84c37ae12935ba7eeea73
Frank Köhn <frankkohn@Franks-MBP.SpeedportEntry2090126250021>
2017-12-19 14:22:40 +0100

    Fix

--- app/Message.php
+++ app/Message.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    protected $fillable = ['message'];
+    protected $fillable = ['message', 'chatroom_id', 'user_id'];
 
     public function user()
     {
