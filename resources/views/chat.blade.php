928bc02077d3db259e7ddd991a64986c6dbc6887
Frank Köhn <frankkohn@Franks-MBP.SpeedportEntry2090126250021>
2017-12-19 15:13:18 +0100

    fix

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -20,6 +20,7 @@
         cluster: 'eu',
         encrypted: true,
         authEndpoint: 'https://pr0jectzer0.ml/broadcasting/auth',
+        host: 'https://pr0jectzer0.ml',
         auth: {
             headers: {
                 'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEsImlzcyI6Imh0dHBzOi8vcHIwamVjdHplcjAubWwvYXBpL3VzZXIvbG9naW4iLCJpYXQiOjE1MTM2ODgwNzEsImV4cCI6MTUxMzY5MTY3MSwibmJmIjoxNTEzNjg4MDcxLCJqdGkiOiJXQlJjZkdWcmVsZHZjMXE1In0.jiRlAAVAGs2OiXCXc0MStUoAywUYffbpHWlxgXHZ6qc'
@@ -28,7 +29,7 @@
     });
 
     //Subscribe to the channel we specified in our Laravel Event
-    var channel = pusher.subscribe('https://pr0jectzer0.ml/private-chat.2');
+    var channel = pusher.subscribe('private-chat.2');
 
     //Bind a function to a Event (the full Laravel class)
     channel.bind('App\\Events\\MessageSent', addMessage);
